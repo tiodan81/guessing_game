e321a586b7aa19918833b267476f017c33bf691c
Dan Schwartz <dpschwartz@gmail.com>
2015-10-28 14:11:45 -0700

    Refactor game code to extend all previous functionality to questions with numerical answers. Edit index.html to point to game_v2.js instead of game.js.

---
+++ game_v2.js
@@ -0,0 +1,68 @@
+var name = prompt('What is your name?');
+
+console.log('Username: ' + name);
+
+alert('Glad you want to play the game, ' + name + '. We think you\'ll find it quite edutaining. Let\'s get started.');
+
+var questions = ['Is my middle name Paul', 'Do I play the guitar', 'Am I left-handed', 'How old am I'];
+var answers = ['yes', 'yes', 'yes', 34]
+var correctAnswers = 0;
+var corrections = ['my middle name is Paul.', 'I do play the guitar.', 'I am left-handed.', 'I am 34 years old.'];
+var clarifications = ['To clarify:'];
+
+var quiz = function () {
+  for (var i = 0; i < questions.length;) {
+    var response = prompt(questions[i] + '?');
+    if (typeof(answers[i]) === 'string' && typeof(response) === 'string') {
+      response = response.toLowerCase();
+      if (response === 'yes' || response === 'y') {
+        correctAnswers += 1;
+        console.log(questions[i] + '? User responded "' + response + '". Correct.');
+        i++;
+      } else if (response === 'no' || response === 'n') {
+        clarifications.push(corrections[i]);
+        console.log(questions[i] + '? User responded "' + response + '". Incorrect.');
+        i++;
+      } else {
+        alert('Please respond yes or no.');
+        console.log(questions[i] + '? User responded "' + response + '". Incomprehensible.');
+        i = i;
+      }
+    } else if (typeof(answers[i]) === 'number' && typeof(parseInt(response)) === 'number') {
+      response = parseInt(response);
+      if (response === answers[i]) {
+        correctAnswers += 1;
+        console.log(questions[i] + '? User responded "' + response + '". Correct.');
+        i++;
+      } else if (response > answers[i] || response < answers[i]) {
+        clarifications.push(corrections[i]);
+        console.log(questions[i] + '? User responded "' + response + '". Incorrect.');
+        i++;
+      } else {
+        alert('Please enter a number.');
+        console.log(questions[i] + '? User responded "' + response + '". Incomprehensible.');
+        i = i;
+      }
+    } else {
+      alert("That's not a valid answer.");
+      i = i;
+    }
+  }
+}
+
+quiz();
+
+
+var message = '';
+
+var messageBuilder = function (clarifications) {
+  for (var j = 0; j < clarifications.length; j++) {
+    message += clarifications[j] + ' ';
+  }
+}
+
+if (clarifications.length > 1) {
+  messageBuilder(clarifications);
+}
+
+alert('Good work, you got ' + correctAnswers + ' out of ' + questions.length +' right! ' + message);
