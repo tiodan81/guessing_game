9f5736965bd94e991386b3f6a55027ff82ff3bd6
Dan Schwartz <dpschwartz@gmail.com>
2015-10-28 14:49:51 -0700

    Add sixth question.

--- game_v2.js
+++ game_v2.js
@@ -4,10 +4,10 @@ console.log('Username: ' + name);
 
 alert('Glad you want to play the game, ' + name + '. We think you\'ll find it quite edutaining. Let\'s get started.');
 
-var questions = ['Is my middle name Paul', 'Do I play the guitar', 'Am I left-handed', 'How old am I', 'Do I love biking'];
-var answers = ['yes', 'yes', 'yes', 34, 'yes'];
+var questions = ['Is my middle name Paul', 'Do I play the guitar', 'Am I left-handed', 'How old am I', 'Do I love biking', 'How many eyeballs do I have'];
+var answers = ['yes', 'yes', 'yes', 34, 'yes', 2];
 var correctAnswers = 0;
-var corrections = ['my middle name is Paul.', 'I do play the guitar.', 'I am left-handed.', 'I am 34 years old.', 'I do love biking.'];
+var corrections = ['my middle name is Paul.', 'I do play the guitar.', 'I am left-handed.', 'I am 34 years old.', 'I do love biking.', 'I have two eyeballs.'];
 var clarifications = ['To clarify:'];
 
 var quiz = function () {
